0b3e39cf18ef78b415a36604f78952a9b3a65964
Dasha-99 <dashakrilova.22@mail.ru>
2024-07-26 16:00:21 +0500

    add projects

---
+++ task1/index.js
@@ -0,0 +1,172 @@
+let jobTypeElement = document.getElementById('job-types')
+let inputTextElements = document.querySelectorAll('.form__text');
+let elemensBorders = [...inputTextElements, jobTypeElement];
+let button = document.querySelector('.form__button');
+
+
+elemensBorders.forEach((input) => {
+    input.addEventListener('focus', function () {
+        input.style.outline = 'none';
+        input.style.border = '2px solid rgb(103, 25, 25)';
+    })
+    input.addEventListener('blur', function () {
+        input.style.border = '';
+    })
+})
+
+const formElements = Array.from(document.querySelector('.form').elements);
+formElements.forEach((input) => {
+    input.addEventListener('input', function () {
+        validationForm(input);
+    })
+})
+
+function validationName() {
+    let nameElement = document.getElementById('name');
+    let nameRegex = /^[a-zа-яё ]+$/i;
+    let nameValue = nameElement.value.trim();
+    if (!nameRegex.test(nameValue) || nameValue.length < 2 || nameValue.length > 20) {
+        return false;
+    }
+    return true;
+}
+
+function validationEmail() {
+    let emailElement = document.getElementById('email');
+    let emailRegexp = /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i;
+    let emailValue = emailElement.value.trim();
+    if (!emailRegexp.test(emailValue)) {
+        return false;
+    }
+    return true;
+}
+
+
+function validationAge() {
+    let ageElement = document.getElementById('age');
+    let ageValue = Number(ageElement.value);
+    if (ageValue <= 0 || ageValue >= 130) {
+        return false;
+    }
+    return true;
+}
+
+function validationJobType() {
+    let jobTypeValue = jobTypeElement.value;
+    if (jobTypeValue === 'default') {
+        return false
+    }
+    return true
+
+}
+
+function validationPassword() {
+    let passwordElement = document.getElementById('password');
+    let passwordRegex = /(?=.*[A-Z])(?=.*[a-z])(?=.*\d)/;
+    let passwordValue = passwordElement.value.trim();
+    if (!passwordRegex.test(passwordValue) || passwordValue.length < 8) {
+        return false;
+    }
+    return true;
+}
+
+function validationCheckbox() {
+    let checkboxElement = document.querySelector('.form__checkbox');
+    let checkboxValue = checkboxElement.checked;
+    if (!checkboxValue) {
+        return false
+    }
+    return true
+}
+
+
+function validationForm(input) {
+    let resultFieldValidation = true;
+    switch (input.name) {
+        case 'name':
+            resultFieldValidation = validationName()
+            break;
+        case 'email':
+            resultFieldValidation = validationEmail()
+            break;
+        case 'age':
+            resultFieldValidation = validationAge()
+            break;
+        case 'job-types':
+            resultFieldValidation = validationJobType()
+            break;
+        case 'password':
+            resultFieldValidation = validationPassword()
+            break;
+        case 'checkbox':
+            resultFieldValidation = validationCheckbox()
+            break;
+        default:
+            return;
+    }
+
+    let fieldError = document.querySelector(`.form__error--${input.name}`);
+    if (resultFieldValidation) {
+        fieldError.style.display = 'none';
+    }
+    else {
+        fieldError.style.display = 'block';
+    }
+
+    checkAllFields();
+}
+
+function checkAllFields() {
+    if (validationName() && validationEmail() && validationAge() && validationJobType() && validationPassword() && validationCheckbox()) {
+        button.removeAttribute('disabled');
+        return true
+    }
+    else {
+        button.setAttribute('disabled', "true");
+        return false
+    }
+}
+
+let form = document.querySelector('.form');
+form.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+    if (checkAllFields()) {
+
+        printFormFields(formElements);
+        form.reset();
+
+    }
+})
+
+function printFormFields(formFields) {
+    for (let temp of formFields) {
+        switch (temp.name) {
+            case 'name':
+                console.log(`Имя: ${temp.value.trim()}`)
+                break;
+            case 'email':
+                console.log(`E-Mail: ${temp.value.trim()}`)
+                break;
+            case 'age':
+                console.log(`Возраст: ${temp.value}`)
+                break;
+            case 'sex':
+                if (temp.checked) {
+                    console.log(`Пол: ${temp.value}`)
+                }
+                break;
+            case 'job-types':
+                console.log(`Профессия: ${temp.value}`)
+                break;
+            case 'password':
+                console.log(`Пароль: ${temp.value}`)
+                break;
+            case 'checkbox':
+                console.log(`Согласие с обработкой данных: ${temp.checked}`)
+                break;
+            default:
+                break;
+        }
+
+    }
+}
---
+++ task2/index.js
@@ -0,0 +1,222 @@
+let brandCar = document.getElementById('brand-car');
+brandCar.addEventListener('change', changeCarModel);
+
+let usedCar = document.getElementById('used-car');
+let newCar = document.getElementById('new-car');
+usedCar.addEventListener('change', selectedCondition);
+newCar.addEventListener('change', selectedCondition);
+
+let calculateForm = document.querySelector('.calculator__form');
+calculateForm.addEventListener('submit', calculatePrice);
+
+let engineCapacityElement = document.getElementById('engine-capacity');
+engineCapacityElement.addEventListener('change', validationEngineCapacity);
+
+
+
+function calculatePrice(evt) {
+    evt.preventDefault();
+    deleteResultValues();
+    if (!validationEngineCapacity()) {
+        return
+    };
+    let priceResult = 0;
+    let brandCurrentCar = brandCar.value.toLowerCase();
+    let modelNameCurrentCar = document.getElementById(`${brandCurrentCar}-model`);
+    let modelCurrentCar = modelNameCurrentCar.value.toLowerCase();
+    priceResult = priceByBrandAndModel[`${brandCurrentCar}-${modelCurrentCar}`];
+    let fuelType = getFuelType();
+    priceResult += priceByFuelType[fuelType];
+
+    let engineCapacity = Number(engineCapacityElement.value);
+    engineCapacity = engineCapacity === 0 ? 1.1 : engineCapacity;
+    priceResult += engineCapacity * 100000;
+    let carCondition = newCar.value;
+    let amounOwners = '0';
+    if (usedCar.checked) {
+        carCondition = usedCar.value;
+        let amountOwnersElements = document.querySelectorAll('.calculator__amount-owners');
+        for (let item of amountOwnersElements) {
+            if (item.checked) {
+                amounOwners = item.value;
+            }
+        }
+    }
+    if (amounOwners === '0') {
+        priceResult += 400000;
+    }
+    else if (amounOwners === 'few-owners') {
+        priceResult += 150000;
+    }
+    else {
+        priceResult += 50000;
+    }
+    console.log(carCondition, amounOwners);
+    let paymentTypeElements = document.querySelectorAll('.calculator__payment-type');
+    let paymentType = paymentTypeElements[0].value;
+    for (let item of paymentTypeElements) {
+        if (item.checked) {
+            paymentType = item.value;
+        }
+    }
+
+    let resultValues = [brandCurrentCar, modelCurrentCar, fuelTypeRussian[fuelType], engineCapacity, carConditionRussian[carCondition], amounOwnersRussian[amounOwners], paymentTypeRussian[paymentType], priceResult];
+    printResultValues(resultValues);
+}
+
+function printResultValues(resultValues) {
+    let resultClassNames = ["result__brand", "result__model", "result__fuel-type",
+        "result__engine-capacity", "result__car-condition", "result__amount-owners", "result__payment-type", "result__price"];
+
+    let resultBlock = document.querySelector('.result');
+    resultBlock.style.display = 'block';
+    for (let temp = 0; temp < resultClassNames.length; temp++) {
+        let tempElement = document.querySelector(`.${resultClassNames[temp]} > span`);
+        tempElement.textContent = resultValues[temp] + ';';
+    }
+}
+
+function deleteResultValues() {
+    let resultClassNames = ["result__brand", "result__model", "result__fuel-type",
+        "result__engine-capacity", "result__car-condition", "result__amount-owners", "result__payment-type", "result__price"];
+    let resultBlock = document.querySelector('.result');
+    resultBlock.style.display = 'none';
+    for (let temp = 0; temp < resultClassNames.length; temp++) {
+        let tempElement = document.querySelector(`.${resultClassNames[temp]} > span`);
+        tempElement.textContent = '';
+    }
+}
+
+function validationEngineCapacity() {
+    let errorMessage = document.querySelector('.calculator__error-message');
+    errorMessage.style.display = 'none';
+    let engineCapacityValue = Number(engineCapacityElement.value);
+    if ((engineCapacityValue >= 1.1) && (engineCapacityValue <= 3.5) || (engineCapacityValue === 0)) {
+        return true
+    }
+    else {
+        errorMessage.style.display = ' block';
+        deleteResultValues();
+        return false
+    }
+
+}
+
+function getFuelType() {
+    let fuelTypeElements = document.querySelectorAll('.calculator__fuel-type');
+    for (let item of fuelTypeElements) {
+        if (item.checked) {
+            return item.value;
+        }
+    }
+}
+
+
+function selectedCondition(evt) {
+    let amountOwnersBlock = document.querySelector('.calculator__owners');
+    if (evt.target.value == 'used-car') {
+        amountOwnersBlock.classList.remove('calculator__field--hidden');
+    }
+    else {
+        amountOwnersBlock.classList.add('calculator__field--hidden');
+    }
+}
+
+
+function changeCarModel() {
+    let modelBlocks = document.querySelectorAll('.calculator__model');
+    modelBlocks.forEach((item) => {
+        item.classList.add('calculator__field--hidden')
+    })
+    let selectedModel = '';
+    switch (brandCar.value) {
+        case 'reno':
+            selectedModel = document.querySelector('.calculator__model--reno');
+            break;
+        case 'opel':
+            selectedModel = document.querySelector('.calculator__model--opel');
+            break;
+        case 'mazda':
+            selectedModel = document.querySelector('.calculator__model--mazda');
+            break;
+        case 'jaguar':
+            selectedModel = document.querySelector('.calculator__model--jaguar');
+            break;
+        default:
+            break;
+    }
+    if (selectedModel) {
+        selectedModel.classList.remove('calculator__field--hidden');
+    }
+
+}
+
+let selectElements = document.querySelectorAll('.calculator__value');
+selectElements.forEach((item) => {
+    item.addEventListener('focus', function () {
+        console.log(item)
+        item.outline = 'none';
+        item.style.borderColor = '2px solid #e70404';
+    })
+    item.addEventListener('blur', function () {
+        item.style.border = '1px solid #414040';
+    })
+    item.addEventListener('mouseover', function () {
+        console.log(item)
+        item.outline = 'none';
+        item.style.border = '2px solid #e70404';
+    })
+    item.addEventListener('mouseout', function () {
+        console.log(item)
+        item.style.border = '';
+    })
+})
+
+const priceByBrandAndModel = {
+    'reno-duster': 500000,
+    'reno-logan': 600000,
+    'reno-sandero': 700000,
+    'reno-megan': 800000,
+    'opel-astra': 450000,
+    'opel-corsa': 550000,
+    'opel-mokka': 650000,
+    'mazda-6': 800000,
+    'mazda-cx-5': 900000,
+    'mazda-3': 1000000,
+    'mazda-5': 1100000,
+    'jaguar-xj': 1200000,
+    'jaguar-xf': 1300000,
+    'jaguar-xe': 1400000,
+    'jaguar-e-pace': 1500000
+};
+
+const priceByFuelType = {
+    'petrol': 0,
+    'diesel': 100000,
+    'gas': 200000,
+    'electricity': 300000
+};
+
+const fuelTypeRussian = {
+    'petrol': 'бензин',
+    'diesel': 'дизель',
+    'gas': 'газ',
+    'electricity': 'электричество'
+};
+
+const carConditionRussian = {
+    'new-car': 'новый',
+    'used-car': 'подержанный'
+};
+
+const amounOwnersRussian = {
+    '0': 0,
+    'few-owners': ' 1-2 владельца',
+    'many-owners': 'более 3-х владельцев'
+};
+
+const paymentTypeRussian = {
+    'bank-card': 'банковской картой',
+    'cash': 'наличными',
+    'legal-person': 'cчет на юридическое лицо'
+};
